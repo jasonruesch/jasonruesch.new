58c8b7d2e9fb2561f325a00e0155682624d03e95
Jason Ruesch <jruesch@degreed.com>
2024-01-21 17:07:28 -0500

    chore(repo): add custom Tailwind CSS library

---
+++ libs/shared/tailwind/src/lib/base/colors.ts
@@ -0,0 +1,16 @@
+import colors from 'tailwindcss/colors';
+
+export default {
+  current: colors.current,
+  inherit: colors.inherit,
+  transparent: colors.transparent,
+  black: colors.black,
+  white: colors.white,
+  stone: colors.stone,
+  amber: colors.amber,
+  orange: colors.orange,
+  green: colors.green,
+  yellow: colors.yellow,
+  red: colors.red,
+  blue: colors.blue,
+};
