431890876b6d24345de873d22a3de29c3c46ecff
Jason Ruesch <jruesch@degreed.com>
2024-01-21 00:29:08 -0500

    chore(repo): initial setup

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# 1.0.0 (2024-01-21)
+
+
+### Features
+
+* **jasonruesch:** add copyright to footer ([6325ac7](https://github.com/jasonruesch/jasonruesch/commit/6325ac7caf04a6365b773618c9d906ebd9afe200))
--- src/app/app.tsx
+++ src/app/app.tsx
@@ -4,9 +4,16 @@ import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
-    <div>
-      <NxWelcome title="Jason Ruesch" />
-    </div>
+    <>
+      <div>
+        <NxWelcome title="Jason Ruesch" />
+      </div>
+      <footer style={{ padding: '2rem 0' }}>
+        <p style={{ textAlign: 'center' }}>
+          &copy; {new Date().getFullYear()} Jason Ruesch. All rights reserved.
+        </p>
+      </footer>
+    </>
   );
 }
 
