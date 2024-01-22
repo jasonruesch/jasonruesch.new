4e45f89953a1d6b2672d189855b6bce468b47d5f
Jason Ruesch <jruesch@degreed.com>
2024-01-21 23:11:37 -0500

    feat(jasonruesch): update page layout

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,11 +1,11 @@
 export function App() {
   return (
     <div className="min-h-dvh flex flex-col p-8 gap-8">
-      <main className="flex-1">
-        <h1 className="text-center">Jason Ruesch</h1>
+      <main className="flex-1 flex items-center justify-center">
+        <h1>Jason Ruesch</h1>
       </main>
-      <footer>
-        <p className="text-center">
+      <footer className="flex items-center justify-center">
+        <p>
           &copy; {new Date().getFullYear()} Jason Ruesch. All rights reserved.
         </p>
       </footer>
