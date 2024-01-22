e9de7743e790a8e80e48a8494e12b23c17d4eaa3
Jason Ruesch <jruesch@degreed.com>
2024-01-21 22:39:18 -0500

    feat(jasonruesch): remove example nx welcome component

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,17 +1,15 @@
-import NxWelcome from './nx-welcome';
-
 export function App() {
   return (
-    <>
-      <div>
-        <NxWelcome title="Jason Ruesch" />
-      </div>
-      <footer className="py-8">
+    <div className="min-h-dvh flex flex-col p-8 gap-8">
+      <main className="flex-1">
+        <h1 className="text-center">Jason Ruesch</h1>
+      </main>
+      <footer>
         <p className="text-center">
           &copy; {new Date().getFullYear()} Jason Ruesch. All rights reserved.
         </p>
       </footer>
-    </>
+    </div>
   );
 }
 
