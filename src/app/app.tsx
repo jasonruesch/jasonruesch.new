5e1b74298a484d55c4527979c93b5ace07dc5070
Jason Ruesch <jruesch@degreed.com>
2024-01-22 11:38:41 -0500

    feat(jasonruesch): add logo and dark colors

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,9 +1,16 @@
+import { Logo } from '@/components';
+
 export function App() {
   return (
     <div className="min-h-dvh flex flex-col p-8 gap-8">
+      <header className="flex items-center justify-center">
+        <Logo className="h-8 w-8" />
+      </header>
+
       <main className="flex-1 flex items-center justify-center">
         <h1>Jason Ruesch</h1>
       </main>
+
       <footer className="flex items-center justify-center">
         <p>
           &copy; {new Date().getFullYear()} Jason Ruesch. All rights reserved.
