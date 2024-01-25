bf76c6736e85f7d4e82f4ccbd307a0009a3a70a3
Jason Ruesch <jruesch@degreed.com>
2024-01-24 22:43:36 -0500

    feat(jasonruesch): add antialiased and update gradient

--- libs/ui/src/lib/home/home.tsx
+++ libs/ui/src/lib/home/home.tsx
@@ -1,7 +1,7 @@
 export function Home() {
   return (
     <div className="flex flex-1 flex-col items-center justify-center">
-      <h1 className="max-w-screen-md bg-gradient-to-r from-primary-500 to-secondary-500 bg-clip-text pb-4 text-center text-6xl uppercase !leading-[1cap] text-transparent md:text-7xl dark:from-primary-400 dark:to-secondary-400">
+      <h1 className="max-w-screen-md bg-gradient-to-br from-primary-500 from-30% to-secondary-500 to-70% bg-clip-text pb-4 text-center text-6xl uppercase !leading-[1cap] text-transparent md:text-7xl dark:from-primary-400 dark:to-secondary-400">
         I'm Jason Ruesch, a Senior Frontend Software Engineer
       </h1>
       <p className="max-w-screen-md text-center text-xl text-neutral-500 md:text-2xl dark:text-neutral-400">
